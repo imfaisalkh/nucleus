7b1b9b5b1f53001bc36fed147d56070190fc3c7d
Faisal Khurshid <imfaisalkh@gmail.com>
2021-03-04 14:56:48 +0500

    Fixed Footer Styling Issue.

--- partials/scaffolding/header/button.php
+++ partials/scaffolding/header/button.php
@@ -5,7 +5,7 @@
 
 ?>
 
-<a class="button" href="<?php echo esc_url($button_link); ?>">
+<a class="button element" href="<?php echo esc_url($button_link); ?>" data-visibility="<?php echo esc_attr(isset($element['visibility']) ? $element['visibility'] : 'desktop'); ?>">
     <?php echo esc_html($button_text); ?>
     <ion-icon name="arrow-forward-outline"></ion-icon>
 </a>
